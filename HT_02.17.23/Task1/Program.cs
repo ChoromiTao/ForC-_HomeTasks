66f7d4788f18e5f2653a7bfbb003c9636fc5719b
Choromi Tao <princessailish94@mail.ru>
2023-02-17 22:47:12 +0900

    C# HT, task 3

--- HT_02.17.23/Task1/Program.cs
+++ HT_02.17.23/Task1/Program.cs
@@ -6,3 +6,21 @@
 
 // 23432 -> да
 
+Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+char[] arr = num.ToString().ToCharArray(); 
+
+
+if (num / 10000 == 0 || num / 10000 > 9)
+    Console.WriteLine("Неправильно введено число!");
+else
+    Polindrom(arr);
+
+void Polindrom(char[] array) 
+{
+
+    if(array[0] == array[4] && array[1] == array[3])
+            System.Console.WriteLine("Это палиндром");
+    else
+            System.Console.WriteLine("Это не палиндром");
+    }
--- HT_02.17.23/Task2/Program.cs
+++ HT_02.17.23/Task2/Program.cs
@@ -5,6 +5,28 @@
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
 
+//Альтернатива:
+
+// Console.WriteLine("Введите х1");
+// double x1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите у1");
+// double y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите z1");
+// double z1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите х2");
+// double x2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите у2");
+// double y2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите z1");
+// double z2 = Convert.ToInt32(Console.ReadLine());
+
+// double x3 = Math.Pow(x1-x2, 2);
+// double y3 = Math.Pow(y1-y2, 2);
+// double z3 = Math.Pow(z1-z2, 2);
+
+// Console.WriteLine($"Oтрезок {Math.Sqrt(x3 + y3 + z3)}");
+
 Console.WriteLine("Введите координаты точки А");
 double Ax = Convert.ToInt32(Console.ReadLine());
 double Ay = Convert.ToInt32(Console.ReadLine());
