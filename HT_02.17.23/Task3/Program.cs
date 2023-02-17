1aa9ef65f515dc322f7e197cc50e695c114d404e
Choromi Tao <princessailish94@mail.ru>
2023-02-17 18:11:56 +0900

    C# HT, task 2

--- HT_02.17.23/Task3/Program.cs
+++ HT_02.17.23/Task3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine(i*i*i);
+}
