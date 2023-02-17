72775b52582402026abeea703d2451c76f77fcc7
Choromi Tao <princessailish94@mail.ru>
2023-02-17 23:04:15 +0900

    C# older HT, task 1,2,3

---
+++ HT_02.14.23/Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+char[] arr = num.ToString().ToCharArray();
+int count = 0;
+Console.WriteLine();
+
+FindNumber(arr);
+
+void ReadArray(char[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+        count = i;
+        count++;
+    }
+}
+
+void FindNumber(char[] array)
+
+{
+    ReadArray(arr);
+
+    Console.WriteLine();
+
+    if (count > 2)
+
+         Console.WriteLine(arr[2]);
+
+    else 
+
+        Console.WriteLine("Третьего числа нет");
+
+    }
---
+++ HT_02.14.23/Task3/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача 15 Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите номер дня недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+string nameOfTheDay = " ";
+
+switch (dayNumber)
+{
+  case 1:
+    nameOfTheDay = "Monday";
+    break;
+  case 2:
+    nameOfTheDay = "Tuesday";
+    break;
+  case 3:
+    nameOfTheDay = "Wednesday";
+    break;
+  case 4:
+    nameOfTheDay = "Thursday";
+    break;
+  case 5:
+    nameOfTheDay = "Friday";
+    break;
+  case 6:
+    nameOfTheDay = "Saturday";
+    break;
+  case 7:
+    nameOfTheDay = "Sunday";
+    break;
+  default:
+    Console.WriteLine("В неделе только семь дней");
+    break;
+}
+    Console.WriteLine(nameOfTheDay);
+
+    if (dayNumber == 6 || dayNumber == 7)
+        Console.WriteLine("Это выходной! Гуляем!");
+    else if (dayNumber < 6 && dayNumber > 0)
+        Console.WriteLine("Сегодня будни... Кошмар...");
+    else 
+        Console.WriteLine("От работы не убежишь!");
