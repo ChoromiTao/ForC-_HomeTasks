3b53bad2ebe83abd67a94cfc79a8fd0dd13acf3d
Choromi Tao <princessailish94@mail.ru>
2023-03-03 22:09:35 +0900

    HT from 03/03, not done yet the second task!

---
+++ HT_03.03.23/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+double[,] array = new double[new Random().Next(5,10), new Random().Next(5,10)];
+double number;
+Random rnd = new Random();
+
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    Console.WriteLine();
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        number = new Random().NextDouble();
+        array[i, j] = number* new Random().Next(10,100);
+        Console.Write($"{array[i, j]:F2}" + " ");
+    }
+}
---
+++ HT_03.03.23/Task2/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+double[,] array = new double[new Random().Next(5,10), new Random().Next(5,10)];
+double number;
+Random rnd = new Random();
+
+double[,] GetArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            number = new Random().NextDouble();
+            array[i, j] = number* new Random().Next(10,100);
+            Console.Write($"{array[i, j]:F3}" + "   ");
+        }
+    }
+    Console.WriteLine();
+    return array;
+}
+
+double ArithmeticMean(double[,] array)          // Тебе нужно доделать массив в массиве с арифметическими суммами! тебе нужны среднеарифметические в каждом столбце и запомнить их!
+{
+    double[] ArrayArithmeticMean = new double[array.GetLength(0)];
+    double ArithSum = 0;
+    double ArithDev = 1;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            ArrayArithmeticMean[i] += array[i,j];
+        }
+        Console.Write($"{ArrayArithmeticMean[i]:F3}" + "    ");
+        ArithSum += ArrayArithmeticMean[i];
+
+        Console.WriteLine($"The summary of column {i+1}: {ArithSum:F3}");
+    }
+    Console.WriteLine(" ");
+    ArithDev = ArithSum / array.GetLength(1);
+    Console.WriteLine();
+    Console.WriteLine($"{ArithDev:F3}");
+    return ArithDev;
+}
+
+GetArray(array);
+ArithmeticMean(array);
